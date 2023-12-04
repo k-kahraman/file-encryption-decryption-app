602637835f27b27335746b798fa7e0531da68aaa
k-kahraman <53380398+k-kahraman@users.noreply.github.com>
2023-12-04 18:14:59 +0000

    Fixed the issues that were causing RSA to not work as intended.

--- rsa.py
+++ rsa.py
@@ -16,30 +16,31 @@ def greatest_common_divisor(num1, num2):
     while num2 != 0:
         num1, num2 = num2, num1 % num2
     return num1
-
-
-def find_multiplicative_inverse(exponent, phi_value):
-    inverse = 0
-    previous_inverse = 0
-    next_inverse = 1
-    current_phi_value = phi_value
+        
+def multiplicative_inverse(exponent, totient):
+    multiplicative_inv = 0
+    prev_coefficient = 0
+    current_coefficient = 1
+    new_coefficient = 1
+    working_totient = totient
 
     while exponent > 0:
-        quotient = current_phi_value // exponent
-        remainder = current_phi_value - quotient * exponent
-        current_phi_value = exponent
+        quotient = working_totient // exponent
+        remainder = working_totient - quotient * exponent
+        working_totient = exponent
         exponent = remainder
 
-        temp_inverse = next_inverse - quotient * previous_inverse
-        next_temp_inverse = inverse - quotient * current_phi_value
+        next_coefficient = current_coefficient - quotient * prev_coefficient
+        update_coefficient = multiplicative_inv - quotient * new_coefficient
 
-        next_inverse = previous_inverse
-        previous_inverse = temp_inverse
-        inverse = current_phi_value
-        current_phi_value = next_temp_inverse
+        current_coefficient = prev_coefficient
+        prev_coefficient = next_coefficient
+        multiplicative_inv = new_coefficient
+        new_coefficient = update_coefficient
 
-    if current_phi_value == 1:
-        return inverse + phi_value
+    if working_totient == 1:
+        return multiplicative_inv + totient
+
 
 
 def is_prime(number):
@@ -68,20 +69,23 @@ def generate_key_pair(prime1, prime2):
         exponent = random.randrange(1, phi_value)
         gcd_value = greatest_common_divisor(exponent, phi_value)
 
-    private_key = find_multiplicative_inverse(exponent, phi_value)
+    private_key = multiplicative_inverse(exponent, phi_value)
     return [[exponent, n_value], [private_key, n_value]]
 
 
-def encrypt(public_key, plaintext):
-    key, n_value = public_key
-    cipher = [pow(ord(character), key, n_value) for character in plaintext]
+def encrypt(pk, plaintext):
+    key, n = pk
+    # Convert each letter in the plaintext to numbers based on the character using a^b mod m
+    cipher = [pow(ord(char), key, n) for char in plaintext]
+    # Return the array of bytes
     return cipher
 
 
-def decrypt(private_key, ciphertext):
-    key, n_value = private_key
-    plaintext_characters = [
-        str(pow(char, key, n_value)) for char in ciphertext
-    ]
-    plaintext = [chr(int(char_value)) for char_value in plaintext_characters]
-    return ''.join(plaintext)
+def decrypt(pk, ciphertext):
+    # Unpack the key into its components
+    key, n = pk
+    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    aux = [str(pow(char, key, n)) for char in ciphertext]
+    # Return the array of bytes as a string
+    plain = [chr(int(char2)) for char2 in aux]
+    return ''.join(plain)
